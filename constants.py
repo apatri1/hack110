b3e760f4b8deac87f2c7b407c54fac48863ce8e0
Ashley Artica-Zuniga <apatri@edu.unc>
2023-12-04 09:08:38 -0500

    Co-authored-by: Ana Restrepo <analrest@users.noreply.github.com>

---
+++ constants.py
@@ -0,0 +1,21 @@
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+TICK_RATE = 60
+
+BACKGROUND_WIDTH = 2700
+BACKGROUND_HEIGHT = 600
+
+# Time between enemies is in ms
+NUM_OF_ENEMIES = 100
+TIME_BETWEEN_ENEMIES = 1000
+ENEMY_SPEED = 3
+
+BACKGROUND_SPEED = 1
+PLAYER_SPEED = 6
+
+MUSIC_FILENAME = "assets/8bitmusic.mp3"
+ENEMY_FILENAME = "assets/Enemy_Player.png"
+PLAYER_FILENAME = "assets/UNC_Player.png"
+BACKGROUND_FILENAME = "assets/wood_background.jpg"
+WIN_SOUND_FILENAME = "assets/win_sound.mp3"
+KEY_FILENAME = "assets/key.webp"
